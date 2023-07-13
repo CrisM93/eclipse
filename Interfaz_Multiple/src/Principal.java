2703b4e2c669ed7a1cb698e14856e29b428016cb
CrisM93 <criztian.gio@gmail.com>
2023-07-12 21:26:13 -0500

    Tercer Commit

--- Interfaz_Multiple/src/Principal.java
+++ Interfaz_Multiple/src/Principal.java
@@ -4,13 +4,16 @@ public class Principal {
 	public static void main(String[] args) {
 
 		Ingeniero ing = new Ingeniero("Pedro", 22, 1500);
-		System.out.println(ing.getNombre() + " ," + ing.getEdad() + " Salario: ," + ing.getSalario());
+		System.out.println("Ingeniero: " + ing.getNombre() + " ," + ing.getEdad() + " Salario: ," + ing.getSalario());
 
 		Antropologo antrop = new Antropologo("Juan", 25, 3400);
-		System.out.println(antrop.getNombre() + " , " + antrop.getEdad() + " Salario: ," + antrop.getSalario());
-		
-		Mariachi mariac =new Mariachi("Martin", 29, 2500);
-		System.out.println(mariac.getNombre() + " , " + mariac.getEdad() + " Salario: ," + mariac.getSalario());
+		System.out.println(
+				"Antropologo: " + antrop.getNombre() + " , " + antrop.getEdad() + " Salario: ," + antrop.getSalario());
+
+		Mariachi mariac = new Mariachi("Martin", 29, 2500);
+		System.out.println("Mariachi: " + mariac.getNombre() + " , " + mariac.getEdad() + " Salario: ," + mariac.getSalario());
+
+		// Son pruebas de github, asi como ejmplode interface java
 
 	}
 
